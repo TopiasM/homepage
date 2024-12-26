b8ba5b1cf50eb9369862f8ff4769689670a4f802
TopiasM <t0de@hotmail.fi>
2024-12-27 02:53:06 +0900

    grainy background + gradient hello text

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,9 @@
+
+export default function Welcome() {
+  
+  return (
+    <div className="flex p-[5vw]">
+      <h1 className="font-semibold text-7xl tracking-wide" id="hello-heading">Hello!</h1>
+    </div>
+  )
+}
